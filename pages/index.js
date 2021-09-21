a7d7a81652cd081be3ab3592c5940defc481a28e
zoutigo <zoutigo@gmail.com>
2021-09-21 16:21:24 +0200

    add cards images

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,49 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
+import data from '../utils/datas'
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product1</li>
-          <li>Product1</li>
-          <li>Product1</li>
-          <li>Product1</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  />
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>€ {product.price}</Typography>
+                  <Button size="small" color="primary">
+                    {' '}
+                    Ajouter
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   )
