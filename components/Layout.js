778bb426ba95fb0f9b4add58049edf6db95b5f8c
zoutigo <zoutigo@gmail.com>
2021-09-21 15:05:31 +0200

    add style

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import useStyles from '../utils/styles'
 
 function Layout({ children }) {
+  const classes = useStyles()
   return (
     <div>
       <Head>
         <title>E Commerce</title>
       </Head>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.navbar}>
           <Typography>e commerce</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>Tout droits reservés. e commerce</Typography>
       </footer>
     </div>
