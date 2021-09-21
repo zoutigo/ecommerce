3387ae96727993eda1bdd1fa3155b2f057a16898
zoutigo <zoutigo@gmail.com>
2021-09-21 15:49:26 +0200

    fix mui styles configuration

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+  }
+
+  const initialProps = await Document.getInitialProps(ctx)
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
+
+export default MyDocument
